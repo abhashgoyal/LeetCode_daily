1aedc4ff1eee01982c955da31471fb9b7c49547f
Abhash Goyal <101199500+abhashgoyal@users.noreply.github.com>
2024-09-17 14:10:25 +0530

    [1] [Time Beats: 63.00%] [Memory Beats: 44.10%] - LeetPush

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                //   while(i!=j){
-                if((nums[i]+nums[j])==target){
-                    return{i,j};
 
-            }
+        /////////////////O(n*N)
+        // int a=0;
+        // int b=0;
+        // for(int i = 0 ; i < nums.size();i++){
+        //     for(int j = i+1; j< nums.size();j++){
+        //         if(nums[i]+nums[j]==target){
+        //             a=i;
+        //             b=j;
+        //         }
+        //     }
+        // }
+        // return {a,b};
+        
+        //// Two pointer
+        
+        unordered_map<int,int> mp;
+        vector<int>ans;
 
-            
-               
+        for(int i = 0;i<nums.size();i++){
+            int a = target-nums[i];
+            if(mp.count(a)){
+                ans.push_back(i);
+                ans.push_back(mp[a]);
+                return ans;
             }
-         
+            mp[nums[i]]=i;
         }
-        return {};
-  
+        return ans;
+
+
+
+        
     }
-   
- };
+};
